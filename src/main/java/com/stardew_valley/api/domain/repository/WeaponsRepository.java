eec12e3995a0ffe52fa64e5db303f2a0da75d6b6
joaoAmaralB <109560143+joaoAmaralB@users.noreply.github.com>
2023-10-24 13:24:31 +0000

    Adding tools and weapons repositories

---
+++ src/main/java/com/stardew_valley/api/domain/repository/WeaponsRepository.java
@@ -0,0 +1,11 @@
+package com.stardew_valley.api.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.stardew_valley.api.domain.model.Weapons;
+
+@Repository
+public interface WeaponsRepository extends JpaRepository<Weapons, Long> {
+    
+}
